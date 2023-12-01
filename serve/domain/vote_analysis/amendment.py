ad445bf728e2c3f86d5f6f4b50e43972353221fe
AchilleSoulie <94848267+AchilleSoulie@users.noreply.github.com>
2023-12-01 17:56:15 +0100

    fix: fix vote analysis domain tests (#4)

--- serve/domain/vote_analysis/amendment.py
+++ serve/domain/vote_analysis/amendment.py
@@ -1,12 +1,13 @@
 
 import re
 from abc import abstractmethod
-from typing import List
+from typing import Dict, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
-from serve.domain.vote_analysis.minutes import Minutes
+from serve.domain.vote_analysis.mep import MEP
 from serve.domain.vote_analysis.page import Page
+from serve.domain.vote_analysis.vote import Vote
 
 
 class Amendment(BaseModel):
@@ -22,9 +23,70 @@ class Amendment(BaseModel):
         original_votes = re.split(correction_regex, full_text)[0]
         return original_votes
 
+class Minutes(BaseModel):
+    id: str
+    pages_list: List[Page]
+    amendments_list: List[Amendment] = []
+
+    def _get_amendment_infos_from_body(self) -> Dict:
+        # read for every amendment its name, id in the document and first page number
+        amendment_infos = {}
+        previous_amendment_name = None
+        for page in sorted(self.pages_list):
+            if page.header is not None:
+                amendment_name = page.extract_amendment_name_from_header()
+                amendment_infos[amendment_name] = [page]
+                previous_amendment_name = amendment_name
+            elif previous_amendment_name is not None:
+                amendment_infos[amendment_name].append(page)
+        return amendment_infos
+
+    @model_validator(mode="after")
+    def extract_amendments(self) -> "Minutes":
+        amendment_infos = self._get_amendment_infos_from_body()
+        for amendment_name, pages in amendment_infos.items():
+            self.amendments_list.append(
+                Amendment(id=amendment_name, minutes_id=self.id, pages=pages)
+            )
+        return self
+
 
 class Amendments(BaseModel):
 
     @abstractmethod
     def save_amendments(self, minutes: Minutes, amendment_ids: List[str]):
         raise NotImplementedError
+
+
+class MinutesAggregate(BaseModel):
+
+    @staticmethod
+    def extract_votes_from_amendments(minutes: Minutes, amendment_ids: List[str]) -> List[Vote]:
+        amendments = [x for x in minutes.amendments_list if x.id in amendment_ids]
+        extracted_votes = []
+        for amendment in amendments:
+            original_votes = amendment._get_str_amendment_votes()
+            # split vote by category (for, against, null)
+            regex_vote_categories = r"(\d+)([+0-])"
+            votes_by_category = re.split(regex_vote_categories, original_votes)[1:]
+
+            # for every vote category, extract the MEP
+            regex_group = r"\n+?(.+?): *"
+            for i in range(0, len(votes_by_category), 3):
+                expected_category_total_votes = int(votes_by_category[i])
+                vote_category = votes_by_category[i + 1]
+                category_votes = votes_by_category[i + 2]
+                votes_by_group = re.split(regex_group, category_votes)
+                extracted_category_votes = 0
+                for i in range(1, len(votes_by_group), 2):
+                    current_group_short_name = votes_by_group[i]
+                    group_voters = votes_by_group[i + 1].replace('\n', '').split(', ')
+                    for name in group_voters:
+                        mep = MEP(
+                            name=name,
+                            current_group_short_name=current_group_short_name
+                        )
+                        extracted_votes.append(Vote(amendment_id=amendment.id, value=vote_category, mep=mep))
+                        extracted_category_votes += 1
+                assert expected_category_total_votes == extracted_category_votes  # transformer exception métier
+        return extracted_votes
